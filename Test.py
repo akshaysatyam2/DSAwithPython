083f7a21014aec5e0881a36aa0cd27aa6919476b
akshaysatyam2 <akshaysatyam2003@gmail.com>
2023-04-16 22:43:52 +0530

    Added Bi-Directional Graph and renamed HashTable to Hash Map for DSA series.

--- Test.py
+++ Test.py
@@ -1,22 +1,31 @@
 # print("Hello DSA, this file if for  testing purpose only.")
-class a:
-    def aa(self):
-        print("A")
+# class a:
+#     def aa(self):
+#         print("A")
+#
+#
+# class b(a):
+#     def bb(self):
+#         print("B")
+#
+#
+# class c(b):
+#     def cc(self):
+#         print("C")
+#
+#
+# if __name__=="__main__":
+#         cob=c()
+#
+#         cob.cc()
+#         cob.bb()
+#         cob.aa()
+
+# Testing for recursion
+def fact(n):
+    if n == 1:
+        return 1
+    return n * fact(n - 1)
 
 
-class b(a):
-    def bb(self):
-        print("B")
-
-
-class c(b):
-    def cc(self):
-        print("C")
-
-
-if __name__=="__main__":
-        cob=c()
-
-        cob.cc()
-        cob.bb()
-        cob.aa()
+print(fact(5))
--- rBST_contains.py
+++ rBST_contains.py
@@ -29,16 +29,18 @@ class BinarySearchTree:
                     return True
                 temp = temp.right
 
-    def contains(self, value):
-        temp = self.root
-        while temp is not None:
-            if value < temp.value:
-                temp = temp.left
-            elif value > temp.value:
-                temp = temp.right
-            else:
-                return True
-        return False
+    def __r_contains(self, current_node, value):
+        if current_node is None:
+            return False
+        if value == current_node.value:
+            return True
+        if value < current_node.value:
+            return self.__r_contains(current_node.left, value)
+        if value > current_node.value:
+            return self.__r_contains(current_node.right, value)
+
+    def r_contains(self, value):
+        return self.__r_contains(self.root, value)
 
 
 my_tree = BinarySearchTree()
@@ -51,7 +53,7 @@ my_tree.insert(52)
 my_tree.insert(82)
 
 print('BST Contains 27:')
-print(my_tree.contains(27))
+print(my_tree.r_contains(27))
 
 print('\nBST Contains 17:')
-print(my_tree.contains(17))
+print(my_tree.r_contains(17))
